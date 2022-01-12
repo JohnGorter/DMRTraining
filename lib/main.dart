c876b513f7f2486ab7a9bb47d7ac9e704c504175
John Gorter <john.gorter@gmail.com>
2022-01-12 14:04:08 +0100

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,71 @@
+import 'dart:async';
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 main() {
   runApp(MaterialApp(home: MyChart()));
 }
 
-class MyChart extends StatelessWidget {
+class MyChart extends StatefulWidget {
   const MyChart({Key? key}) : super(key: key);
+
+  @override
+  State<MyChart> createState() => _MyChartState();
+}
+
+class _MyChartState extends State<MyChart> {
+  List<int> points = [];
+  bool expanded = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    points = [10, 20, 30, 35, 36, 37, 38, 39, 50];
+    Timer t = Timer.periodic(Duration(seconds: 1), (t) {
+      if (points.length > 50) points.removeAt(0);
+      int p = points.last;
+      int margin = math.Random().nextInt(25);
+      int positive = (math.Random().nextInt(10) % 2);
+      if (positive == 0) p = (p * (100 + margin) / 100).round();
+      if (positive == 1) p = (p * (100 - margin) / 100).round();
+      points.add(math.min(p, 100));
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("whatever")),
+        appBar: AppBar(actions: [
+          IconButton(
+            icon: Icon(Icons.expand),
+            onPressed: () {
+              setState(() {
+                expanded = !expanded;
+              });
+            },
+          )
+        ], title: Text("whatever")),
         body: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height -
-                AppBar().preferredSize.height,
-            child: CustomPaint(painter: MyPainter())));
+            width: expanded ? MediaQuery.of(context).size.width: 200,
+            height: expanded ? MediaQuery.of(context).size.height -
+                AppBar().preferredSize.height : 200,
+            child: CustomPaint(painter: MyPainter(points: points))));
   }
 }
 
 class MyPainter extends CustomPainter {
+  List<int> points;
+  MyPainter({this.points = const []});
+
   @override
   void paint(Canvas canvas, Size size) {
-    List<int> points = [10, 20, 30, 35, 36, 37, 38, 39, 50];
     double margin = 50;
     double linewidth = size.width - (margin * 2);
     double lineheight = size.height - (margin * 2);
-    double intervalx = linewidth / (points.length - 1);
+    double intervalx = linewidth / (math.max(points.length, 50) - 1);
 
     Path p = Path()
       ..moveTo(margin, margin)
@@ -40,12 +79,15 @@ class MyPainter extends CustomPainter {
 
     canvas.drawPath(p, paint);
 
-    for (int i = 0; i < points.length; i++) {
+    for (int i = 0; i < math.max(points.length, 50); i++) {
+      int value = i < points.length ? points[i] : 0;
       double offset = i * intervalx;
-      double offsety = lineheight - (lineheight / 100) * points[i];
-      canvas.drawOval(
-          Rect.fromLTWH(offset + margin - 5, offsety + margin - 5, 10, 10),
-          paint);
+      double offsety = lineheight - (lineheight / 100) * value;
+      if (value > 0) {
+        canvas.drawOval(
+            Rect.fromLTWH(offset + margin - 5, offsety + margin - 5, 10, 10),
+            paint);
+      }
     }
   }
 
